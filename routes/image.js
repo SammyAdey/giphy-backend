126487a9868f6482351b862cde498334af3e3624
Samuel Adeyemi <Samuel3adeyemi@live.com>
2023-11-06 18:33:13 +1000

    image error handleling improved

--- routes/image.js
+++ routes/image.js
@@ -7,9 +7,16 @@ const s3 = new S3({ apiVersion: "2006-03-01" });
 
 router.get("/:key", (req, res) => {
 	const key = req.params.key;
-	const readStream = getFileStream(key);
-
-	readStream.pipe(res);
+	try {
+		const readStream = getFileStream(key);
+		readStream.pipe(res);
+		// throw error if file not found
+		// readStream.on("error", (err) => {
+		// 	throw err;
+		// });
+	} catch (error) {
+		res.status(404).send("File not found");
+	}
 });
 
 //downloads an image from s3
